0883a2502422636b8694eae5cdc083b5eeaed712
Christopher Manahan <chris.a.manahan@gmail.com>
2019-05-16 11:20:14 -0400

    Create the Accounts module to handle Users Implement the Accounts module to handle creation, update, and deletion of User information specifed by the user and credential migrations

---
+++ lib/ebae/accounts/user.ex
@@ -0,0 +1,21 @@
+defmodule Ebae.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Ebae.Accounts.Credential
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    has_one :credential, Credential, on_replace: :update
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :username])
+    |> validate_required([:name, :username])
+    |> unique_constraint(:username)
+  end
+end
